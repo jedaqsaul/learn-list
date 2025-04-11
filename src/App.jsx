c4fe4e9c903f7cd6d348e68ef4234c88bd3c25cd
jedidiah <jedaqsaul@gmail.com>
2025-04-11 22:19:07 +0300

    used .filter to sort the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import React, { useState } from "react";
 import UserContainer from "./user/user-container";
 import users from "../lib";
+import "./App.css";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState(""); // above is the search state
   // filter users based on searchTerm
-  const filteredUsers = users.filter((user) =>
-    user.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
+  const filteredUsers = users.filter(
+    (user) =>
+      user.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      user.email.toLowerCase().includes(searchTerm.toLowerCase())
   );
   return (
     <div className="App">
       <h1> Student List</h1>
-      <UserContainer users={users} />
+      {/* search input goes here */}
+      <input
+        className="search-bar"
+        type="text"
+        placeholder="Search students by name..."
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)}
+      />
+      {/* send filtered users to UserContainer */}
+      {filteredUsers.length > 0 ? (
+        <UserContainer users={filteredUsers} />
+      ) : (
+        <p className="demo">No Matching students found</p>
+      )}
     </div>
   );
 }
